d629406089e1c03e6321ee47ce59f40f5b54aa7f
CadetWu <c26brendan.wu@afacademy.af.edu@users/noreply.github.com>
2024-02-16 00:00:05 -0700

    Still have issues

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -84,6 +84,7 @@ architecture top_basys3_arch of top_basys3 is
   -- declare any signals you will need	
 signal w_S1, w_S2 : std_logic := '0'; 
 signal w_Cout1, w_Cout2 : std_logic := '0';
+
 begin
 -- PORT MAPS --------------------
         halfAdder1_inst: halfAdder
@@ -98,13 +99,13 @@ begin
         port map(
             i_A     => w_S1,
             i_B     => sw(2),
-            o_S     => w_S2,
+            o_S     => led(0),
             o_Cout  => w_Cout2
         );
             ---------------------------------
         
             -- CONCURRENT STATEMENTS --------
-        led(0) <= w_S1;
+        --led(0) <= w_S1;
         led(1) <= (w_Cout1 OR w_Cout2);
             ---------------------------------
 	     
